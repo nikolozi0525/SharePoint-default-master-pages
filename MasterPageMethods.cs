b9e7f86ac0b1590d39aaadb01656376314adef5f
Nikolozi <nikolozi0525@gmail.com>
2023-05-26 15:39:09 -0400

    initial commit

---
+++ MasterPageMethods.cs
@@ -0,0 +1,34 @@
+﻿namespace SharePoint_Default_Master_Pages
+{
+    using System;
+
+    using Microsoft.SharePoint;
+
+    public static class MasterPageMethods
+    {
+        public static void SetMasterPage(this SPFeatureReceiverProperties properties)
+        {
+            properties.SetMasterPage("/_catalogs/masterpage/v4.master");
+        }
+
+        public static void SetMasterPage(this SPFeatureReceiverProperties properties, string url)
+        {
+            properties.SetMasterPage(url, true);
+        }
+
+        public static void SetMasterPage(this SPFeatureReceiverProperties properties, string url, bool setCustomMasterUrl)
+        {
+            var curSite = (SPSite)properties.Feature.Parent;
+            var curWeb = curSite.RootWeb;
+            var masterUri = new Uri(curWeb.Url + url);
+
+            curWeb.MasterUrl = masterUri.AbsolutePath;
+            if (setCustomMasterUrl)
+            {
+                curWeb.CustomMasterUrl = masterUri.AbsolutePath;
+            }
+
+            curWeb.Update();
+        }
+    }
+}
